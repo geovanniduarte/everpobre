04ce66136ef1b3c7fa3c87e3b3274463810fb884
Geo <geovanni.duarte.guerrero@gmail.com>
2018-05-08 18:26:04 -0500

    mas a rotater

--- Everpobre/Controller/Note/UIRotater.swift
+++ Everpobre/Controller/Note/UIRotater.swift
@@ -15,34 +15,60 @@ class UIRotater: UIView {
     
     var sliderRotater = UISlider()
     var sliderZoomer = UISlider()
+    var rotaterValue = UITextView()
+    var zoomerValue = UITextView()
+    
     weak var delegate : UIRotaterDelegate?
 
     init() {
         super.init(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
         self.backgroundColor = .red
+        
+        sliderRotater.maximumValue = 270
         addSubview(sliderRotater)
+        
+        sliderZoomer.maximumValue = 1.3
+        sliderZoomer.minimumValue = 0.7
         addSubview(sliderZoomer)
+        
+        rotaterValue.text = "VALOR: \(sliderRotater.minimumValue)"
+        rotaterValue.backgroundColor = .blue
+        addSubview(rotaterValue)
+        
+        zoomerValue.text = "\(sliderZoomer.minimumValue)"
+        addSubview(zoomerValue)
         
         sliderRotater.translatesAutoresizingMaskIntoConstraints = false
         sliderZoomer.translatesAutoresizingMaskIntoConstraints = false
+        rotaterValue.translatesAutoresizingMaskIntoConstraints = false
+        zoomerValue.translatesAutoresizingMaskIntoConstraints = false
         
-        sliderRotater.maximumValue = 270
-        sliderZoomer.maximumValue = 1.3
-        sliderZoomer.minimumValue = 0.7
+        let viewDict = ["zoomerValue": zoomerValue, "sliderRotater" : sliderRotater, "sliderZoomer" : sliderZoomer, "rotaterValue" : rotaterValue] as [String : Any]
         
-        let viewDict = ["sliderRotater" : sliderRotater, "sliderZoomer" : sliderZoomer]
+        //HORIZONTAL
+        var constrains = NSLayoutConstraint.constraints(withVisualFormat: "|-10-[sliderRotater]-10-[rotaterValue]-10-|", options: [], metrics: nil, views: viewDict)
+        constrains.append(contentsOf: NSLayoutConstraint.constraints(withVisualFormat: "[rotaterValue(100)]", options: [], metrics: nil, views: viewDict))
         
-        var constrains = NSLayoutConstraint.constraints(withVisualFormat: "|-10-[sliderRotater]-10-|", options: [], metrics: nil, views: viewDict)
+        constrains.append(contentsOf: NSLayoutConstraint.constraints(withVisualFormat: "|-10-[sliderZoomer]-10-[zoomerValue]-10-|", options: [], metrics: nil, views: viewDict))
         
-        constrains.append(contentsOf: NSLayoutConstraint.constraints(withVisualFormat: "|-10-[sliderZoomer]-10-|", options: [], metrics: nil, views: viewDict))
-        
+        //VERTICAL
         constrains.append(contentsOf: NSLayoutConstraint.constraints(withVisualFormat: "V:|-10-[sliderRotater]-10-[sliderZoomer]-10-|", options: [], metrics: nil, views: viewDict))
+        
+        sliderRotater.addTarget(self, action: #selector(changeRotationValue), for: .valueChanged)
         
         addConstraints(constrains)
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("This class does not support NSCoding")
+    }
+    
+    @objc func changeRotationValue(sender : UISlider) {
+        delegate?.rotater(self, didChangeRotation: sender.value)
+    }
+    
+    @objc func changeZoomValue(sender : UISlider) {
+        delegate?.rotater(self, didChangeZoom: sender.value)
     }
     
 }
